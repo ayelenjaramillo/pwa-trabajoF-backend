f5f531a2c0a217a61151ddb23d0a166161e3e8e4
Ayelen Jaramillo <ayelenjaramillotw@gmail.com>
2026-06-13 20:09:40 -0300

    configuracion sequelize

--- database/config/database.js
+++ database/config/database.js
@@ -1,14 +1,16 @@
 require("dotenv").config();
 module.exports={
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 3307,
-    dialect: "mysql", 
-    logging: false
+    // username: process.env.DB_USER,
+    // password: process.env.DB_PASS,
+    // database: process.env.DB_NAME,
+    // host: process.env.DB_HOST,
+    // host: process.env.DB_HOST,
+    // port: process.env.DB_PORT || 3307,
+     dialect: "mysql", 
+     logging: false,
+     url: process.env.DB_URL
+
   }
 }
 
--- database/models/index.js
+++ database/models/index.js
@@ -11,12 +11,16 @@ const config = require(__dirname + '/../config/database.js')[env];
 console.log("config: ", config);
 const db = {};
 
-let sequelize;
-if (config?.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+
+const sequelize = new Sequelize(process.env.DB_URL, {
+  dialect: "mysql",
+  logging: false
+});
+// if (config?.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
 
 fs
   .readdirSync(__dirname)
